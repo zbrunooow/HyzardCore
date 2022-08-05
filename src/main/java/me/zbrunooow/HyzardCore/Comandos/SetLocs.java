b97191ede550e3b5a1b0ba056202dc2ffd3d286a
Gabriel Lopes <zbrunooow@gmail.com>
2022-08-04 23:50:33 -0300

    Corrigido (04/08/2022)

--- src/main/java/me/zbrunooow/HyzardCore/Comandos/SetLocs.java
+++ src/main/java/me/zbrunooow/HyzardCore/Comandos/SetLocs.java
@@ -9,43 +9,55 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-public class SetLocs implements CommandExecutor {
+import java.util.ArrayList;
+
+public class SetLocs {
 
     public SetLocs(Core core) {
-        core.getCommand("setspawn").setExecutor(this);
-    }
+        HyzardCommand command = new HyzardCommand(core, "setspawn", "setar o spawn do servidor", "", new ArrayList<>());
+        command.setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender s, Command cmd, String lb, String[] args) {
+                if (!(s instanceof Player)) return true;
+                Player p = (Player) s;
 
-    public boolean onCommand(CommandSender s, Command cmd, String lb, String[] args) {
-        if (!(s instanceof Player)) return true;
-        Player p = (Player) s;
+                if (p.hasPermission("hyzardcore.setspawn") || p.hasPermission("hyzardcore.*")) {
+                    if(args.length == 1) {
+                        if(args[0].equalsIgnoreCase("normal")) {
+                            LocsFile.get().getLocs().set("Spawn.Normal", API.get().serialize(p.getLocation()));
+                            LocsFile.get().saveLocs();
+                            p.sendMessage(command.getMensagens().getMsg("Setou_Spawn").replace("{tipo}", "NORMAL"));
+                            p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
 
-        if(cmd.getName().equalsIgnoreCase("setspawn")) {
-            if (p.hasPermission("hyzardcore.setspawn") || p.hasPermission("hyzardcore.*")) {
-                if(args.length == 1) {
-                    if(args[0].equalsIgnoreCase("normal")) {
-                        LocsFile.get().getLocs().set("Spawn.Normal", API.get().serialize(p.getLocation()));
-                        LocsFile.get().saveLocs();
-                        p.sendMessage("§aVocê setou o spawn NORMAL!");
-                        p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
+                        } else if (args[0].equalsIgnoreCase("vip")) {
+                            LocsFile.get().getLocs().set("Spawn.VIP", API.get().serialize(p.getLocation()));
+                            LocsFile.get().saveLocs();
+                            p.sendMessage(command.getMensagens().getMsg("Setou_Spawn").replace("{tipo}", "VIP"));
+                            p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
 
-                    } else if (args[0].equalsIgnoreCase("vip")) {
-                        LocsFile.get().getLocs().set("Spawn.VIP", API.get().serialize(p.getLocation()));
-                        LocsFile.get().saveLocs();
-                        p.sendMessage("§aVocê setou o spawn VIP!");
-                        p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
-
+                        }
+                    } else {
+                        p.sendMessage(command.getMensagens().getMsg("Como_Usar"));
                     }
                 } else {
-                    p.sendMessage("§cUse (/setspawn [normal/vip])");
+                    p.sendMessage(Mensagens.get().getSemPerm());
                 }
-            } else {
-                p.sendMessage(Mensagens.get().getSemPerm());
+
+                return false;
             }
-        }
+        });
 
+        command.getMensagens().createMensagens(() -> {
+            ConfigurationSection config = command.getMensagens().getConfigurationSection();
+            config.set("Como_Usar", "&cUse (/renomear [nome])");
+            config.set("Setou_Spawn", "§aVocê setou o spawn {tipo}!");
 
-        return false;
+            command.saveConfig();
+            command.getMensagens().loadMensagens();
+        });
+
     }
 }
--- src/main/java/me/zbrunooow/HyzardCore/Comandos/Spawn.java
+++ src/main/java/me/zbrunooow/HyzardCore/Comandos/Spawn.java
@@ -6,55 +6,75 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-public class Spawn implements CommandExecutor {
+import java.util.ArrayList;
+
+public class Spawn {
 
     public Spawn(Core core) {
-        core.getCommand("spawn").setExecutor(this);
-    }
+        HyzardCommand command = new HyzardCommand(core, "spawn", "vai para o ponto de spawn do servidor", "", new ArrayList<>());
+        command.setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender s, Command cmd, String lb, String[] args) {
+                if (!(s instanceof Player)) return true;
+                Player p = (Player) s;
 
-    public boolean onCommand(CommandSender s, Command cmd, String lb, String[] args) {
-        if (!(s instanceof Player)) return true;
-        Player p = (Player) s;
+                if(args.length != 0) {
+                    p.sendMessage(command.getMensagens().getMsg("Como_Usar"));
+                    return false;
+                }
 
-        if(cmd.getName().equalsIgnoreCase("spawn")) {
-            if(p.hasPermission("hyzardcore.spawnvip")) {
-                if(Locations.get().getSpawnVip() != null) {
-                    if (!Locations.get().getSpawnVip().getChunk().isLoaded()) {
-                        Locations.get().getSpawnVip().getChunk().load();
+                if(p.hasPermission("hyzardcore.spawnvip")) {
+                    if(Locations.get().getSpawnVip() != null) {
+                        if (!Locations.get().getSpawnVip().getChunk().isLoaded()) {
+                            Locations.get().getSpawnVip().getChunk().load();
+                        }
+                        p.teleport(Locations.get().getSpawnVip());
+                        p.sendMessage(command.getMensagens().getMsg("Teleportado_Vip"));
+                        p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
+                    } else {
+                        if(Locations.get().getSpawnNormal() != null) {
+                            if (!Locations.get().getSpawnNormal().getChunk().isLoaded()) {
+                                Locations.get().getSpawnNormal().getChunk().load();
+                            }
+                            p.teleport(Locations.get().getSpawnNormal());
+                            p.sendMessage(command.getMensagens().getMsg("Teleportado"));
+                            p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
+                        } else {
+                            p.sendMessage(command.getMensagens().getMsg("Sem_Spawn"));
+                            p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1, 10);
+                        }
                     }
-                    p.teleport(Locations.get().getSpawnVip());
-                    p.sendMessage("§aVocê foi teleportado para o Spawn (VIP).");
-                    p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
                 } else {
                     if(Locations.get().getSpawnNormal() != null) {
                         if (!Locations.get().getSpawnNormal().getChunk().isLoaded()) {
                             Locations.get().getSpawnNormal().getChunk().load();
                         }
                         p.teleport(Locations.get().getSpawnNormal());
-                        p.sendMessage("§aVocê foi teleportado para o Spawn.");
+                        p.sendMessage(command.getMensagens().getMsg("Teleportado"));
                         p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
                     } else {
-                        p.sendMessage("§cO spawn não foi setado.");
+                        p.sendMessage(command.getMensagens().getMsg("Sem_Spawn"));
                         p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1, 10);
                     }
                 }
-            } else {
-                if(Locations.get().getSpawnNormal() != null) {
-                    if (!Locations.get().getSpawnNormal().getChunk().isLoaded()) {
-                        Locations.get().getSpawnNormal().getChunk().load();
-                    }
-                    p.teleport(Locations.get().getSpawnNormal());
-                    p.sendMessage("§aVocê foi teleportado para o Spawn.");
-                    p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 10);
-                } else {
-                    p.sendMessage("§cO spawn não foi setado.");
-                    p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1, 10);
-                }
-            }
-        }
+                return false;
 
-        return false;
+            }
+        });
+
+        command.getMensagens().createMensagens(() -> {
+            ConfigurationSection config = command.getMensagens().getConfigurationSection();
+            config.set("Como_Usar", "&cUse (/renomear [nome])");
+            config.set("Sem_Spawn", "&cO Spawn não foi setado.");
+            config.set("Teleportado", "&aVocê foi teleportado para o Spawn!");
+            config.set("Teleportado_Vip", "&aVocê foi teleportado para o Spawn (VIP)!");
+
+            command.saveConfig();
+            command.getMensagens().loadMensagens();
+        });
+
     }
 }
